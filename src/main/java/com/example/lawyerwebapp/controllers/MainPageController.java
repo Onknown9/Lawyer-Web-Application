12f7b17e3153d99627bd09c0d18198f37d766179
George Smokin <lmaosaurus9897@gmail.com>
2023-09-16 15:54:27 +0200

    Website Added form handler (now after button click info is passed to controller)

--- src/main/java/com/example/lawyerwebapp/controllers/MainPageController.java
+++ src/main/java/com/example/lawyerwebapp/controllers/MainPageController.java
@@ -1,12 +1,30 @@
 package com.example.lawyerwebapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainPageController {
-    @GetMapping("/lawyer")
+    @GetMapping("/")
     public String mainPage(){
+        return "Lawyer";
+    }
+    @PostMapping("/send-message")
+    public String sendMessage(
+            @RequestParam("name") String name,
+            @RequestParam("email") String email,
+            @RequestParam("topic") String topic,
+            @RequestParam("message") String message,
+            Model model){
+
+        model.addAttribute("name", name);
+        model.addAttribute("email", email);
+        model.addAttribute("topic", topic);
+        model.addAttribute("message", message);
+
         return "Lawyer";
     }
 }
